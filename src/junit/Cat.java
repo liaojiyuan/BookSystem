9197960a8941d568b6d9875a7113e2502c0d114e
gaochaochao <gaochaochao@thunisoft.com>
2019-02-27 14:04:09 +0800

    将项目改成maven项目

--- src/junit/Cat.java
+++
@@ -1,8 +0,0 @@
-package junit;
-
-/**
- * Created by ozc on 2017/4/19.
- */
-public class Cat extends Animal {
-
-}
--- src/junit/Dog.java
+++
@@ -1,7 +0,0 @@
-package junit;
-
-/**
- * Created by ozc on 2017/4/19.
- */
-public class Dog extends Animal {
-}
